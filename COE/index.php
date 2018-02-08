bbcd0c594a550ec6cb0ddc0e6157468bbf26064b
nicekajosh22 <aldeithegreat@gmail.com>
2018-02-09 00:04:36 +0800

    Add files via upload

---
+++ COE/index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="header">
+	
+	<main class="clearfix">
+		<header class="col-3">
+			<div class="col-8"><input type="text" name="company name">NAME OF COMPANY</div>
+			<div class="col-8"><input type="text" name="address">ADDRESS/E-MAIL/TELEPHONE NO./FAX NO.</div>
+
+
+	
+		</header>
+		
+
+		
+			<section>
+				<span class="subheading">This is to CERTIFY that MR.MS.<input type="text" name="name of person">is/has been employed with the abover-namedoffice/company located at<input type="tex">for the period and performed duties indicated below: 
+				</span>
+
+			<div class="row">
+				<div class="col-3">
+					<strong>FROM</strong>
+				</div>
+
+				<div class="col-3">
+					<strong>TO</strong>
+				</div>
+
+				<div class="col-3">
+					<strong>POSITION HELD</strong>
+				</div>
+
+				<div class="col-3">
+					<strong>SPECIFIC WORK/FUNCTION</strong>
+				</div>
+			</div> <!-- row -->
+
+			<div class="row">
+				<div class="col-3">
+					
+					<div class="col-8"><input type="text" name=""></div>					 
+				</div>
+
+				<div class="col-3">
+				
+					<div class="col-8"><input type="text" name=""></div>					 
+				</div>
+
+				<div class="col-3">
+				
+					<div class="col-8"><input type="text" name=""></div>					 
+				</div>
+
+				<div class="col-3">
+					
+					<div class="col-8"><input type="text" name=""></div>					 
+				</div>
+			</div> <!-- row -->
+
+			<div class="row">
+				<div class="col-3">
+					
+					<div class="col-8"><input type="text" name=""></div>					 
+				</div>
+
+				<div class="col-3">
+				
+					<div class="col-8"><input type="text" name=""></div>					 
+				</div>
+
+				<div class="col-3">
+					
+					<div class="col-8"><input type="text" name=""></div>					 
+				</div>
+
+				<div class="col-3">
+				
+					<div class="col-8"><input type="text" name=""></div>					 
+				</div>
+			</div> <!-- row -->
+
+			<div class="row">
+				<div class="col-3">
+				
+					<div class="col-8"><input type="text" name=""></div>					 
+				</div>
+
+				<div class="col-3">
+					
+					<div class="col-8"><input type="text" name=""></div>					 
+				</div>
+
+				<div class="col-3">
+				
+					<div class="col-8"><input type="text" name=""></div>					 
+				</div>
+
+				<div class="col-3">
+					
+					<div class="col-8"><input type="text" name=""></div>					 
+				</div>
+			</div> <!-- row -->
+
+			<div class="row">
+				<div class="col-3">
+					
+					<div class="col-8"><input type="text" name=""></div>					 
+				</div>
+
+				<div class="col-3">
+					
+					<div class="col-8"><input type="text" name=""></div>					 
+				</div>
+
+				<div class="col-3">
+					
+					<div class="col-8"><input type="text" name=""></div>					 
+				</div>
+
+				<div class="col-3">
+					
+					<div class="col-8"><input type="text" name=""></div>					 
+				</div>			
+			</div> <!-- row -->
+		</section>	
+
+		<!-- <section >
+			<h4><em>Educational Info </em></h4>
+
+			<div class="row">
+				<div class="col-4 text-right">
+					<strong>Course</strong>
+				</div>
+
+				<div class="col-8">
+					<input type="text">
+				</div>
+			</div> <!-- row -->
+
+			<!-- <div class="row">
+				<div class="col-4 text-right">
+					<strong>Dean</strong>
+				</div> -->
+ <!-- 
+				<div class="col-8">
+					<input type="text">
+				</div>
+			</div> <!-- row -->
+
+			<!-- <div class="row">
+				<div class="col-4 text-right">
+					<strong>Internship Coordinator </strong>
+				</div>
+ -->
+			<!-- 	<div class="col-8">
+					<input type="text">
+				</div>
+			</div> <!-- row -->			
+		<!-- </section -->
+</body>
+</html>
+
+
